0653669ad4f38953a3d281c54802f9cf773727d7
Irfan Kurniawan <irfangokil23@gmail.com>
2022-04-02 13:05:51 +0700

    fix handler editnotes

--- src/handler.js
+++ src/handler.js
@@ -67,11 +67,11 @@ const getNoteByIdHandler = (request, h) => {
 const editNoteByIdHandler = (request, h) => {
   const {id} = request.params;
   const {title, tags, body} = request.payload;
-  const updatedAt = new Date().toISOString;
+  const updatedAt = new Date().toISOString();
 
-  const index = notes.filter((n) => n.id === id)[0];
+  const index = notes.findIndex((note) => note.id === id);
 
-  if (index !== undefined) {
+  if (index !== -1) {
     notes[index] = {
       ...notes[index],
       title,
@@ -82,16 +82,15 @@ const editNoteByIdHandler = (request, h) => {
 
     const response = h.response({
       status: 'success',
-      message: 'Catatan berhasil diubah',
+      message: 'Catatan berhasil diperbarui',
     });
-
     response.code(200);
     return response;
   }
 
   const response = h.response({
     status: 'fail',
-    message: 'Catatan tidak ditemukan',
+    message: 'Gagal memperbarui catatan. Id tidak ditemukan',
   });
   response.code(404);
   return response;
